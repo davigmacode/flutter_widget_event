628e01325d3801cf49fde347ff66b24b19d76787
Irfan Vigma Taufik <davigmacode@gmail.com>
2022-10-10 11:43:47 +0700

    Improve DrivenProperty

--- lib/src/property.dart
+++ lib/src/property.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'event.dart';
 
 /// Signature for the function that returns a value of type `T`
@@ -8,28 +7,45 @@ typedef DrivenPropertyResolver<T> = T Function(Set<WidgetEvent> events);
 /// Interface for classes that [resolve] to a value of type `T`
 /// based on a widget's interactive "event",
 /// which is defined as a set of [WidgetEvent]s.
-class DrivenProperty<T> {
-  DrivenProperty(this.resolver);
-
-  @protected
-  final DrivenPropertyResolver<T> resolver;
+abstract class DrivenProperty<T> {
+  /// Returns a value of type `T` that depends on [events].
+  ///
+  /// Widgets like [TextButton] and [ElevatedButton] apply this method to their
+  /// current [WidgetEvent]s to compute colors and other visual parameters
+  /// at build time.
+  T resolve(Set<WidgetEvent> events);
 
   /// Resolves the value for the given set of events if `value` is a
   /// [DrivenProperty], otherwise returns the value itself.
   ///
   /// This is useful for widgets that have parameters
   /// which can optionally be a [DrivenProperty].
-  static T resolve<T>(T value, Set<WidgetEvent> events) {
+  static T evaluate<T>(T value, Set<WidgetEvent> events) {
     if (value is DrivenProperty<T>) {
       final DrivenProperty<T> property = value;
-      return property.resolver(events);
+      return property.resolve(events);
     }
     return value;
   }
 
   /// Convenience method for creating a [DrivenProperty]
+  /// from a [DrivenPropertyResolver] function alone.
+  static DrivenProperty<T> by<T>(DrivenPropertyResolver<T> callback) {
+    return _DrivenProperty<T>(callback);
+  }
+
+  /// Convenience method for creating a [DrivenProperty]
   /// that resolves to a single value for all events.
   static DrivenProperty<T> all<T>(T value) {
-    return DrivenProperty<T>((events) => value);
+    return _DrivenProperty<T>((events) => value);
   }
+}
+
+class _DrivenProperty<T> implements DrivenProperty<T> {
+  _DrivenProperty(this._resolver);
+
+  final DrivenPropertyResolver<T> _resolver;
+
+  @override
+  T resolve(Set<WidgetEvent> events) => _resolver(events);
 }
